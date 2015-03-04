93b1e4b17a214dac0eff8d2389437e49c3ed8871
Alex Dunae <alex@dunae.ca>
2015-03-04 11:13:31 -0800

    Switch to Soundcloud seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
 Song.destroy_all
 
-puts 'Adding songs'
-Song.create(title: "Song 1", url: '<iframe style="border: 0; width: 100%; height: 42px;" src="http://bandcamp.com/EmbeddedPlayer/album=1688332877/size=small/bgcol=333333/linkcol=fe7eaf/transparent=true/" seamless><a href="http://shop.silentseason.com/album/aeterna">Aeterna by Seraphim Rytm</a></iframe>')
-Song.create(title: "Song 2", url: '<iframe style="border: 0; width: 100%; height: 42px;" src="http://bandcamp.com/EmbeddedPlayer/album=2703797172/size=small/bgcol=333333/linkcol=fe7eaf/transparent=true/" seamless><a href="http://shop.silentseason.com/album/wandering-compilation-sscd06">Wandering Compilation [SSCD06] by Silent Season</a></iframe>')
+soundcloud_data = []
+soundcloud_data << OpenStruct.new(title: 'Deep Electronics #71 - Faidel', track: '189683856')
+soundcloud_data << OpenStruct.new(title: 'SP 109 Deepmeister', track: '176212562')
+soundcloud_data << OpenStruct.new(title: 'Casiokids - Det Haster!', track: '24255336')
+soundcloud_data << OpenStruct.new(title: 'Fetty Wap - Trap Queen', track: '155798913')
+soundcloud_data << OpenStruct.new(title: 'Jon Hopkins - Form by Firelight', track: '170000664')
+soundcloud_data << OpenStruct.new(title: 'Major Lazer & DJ Snake - Lean On (feat. MØ)', track: '193781466')
+soundcloud_data << OpenStruct.new(title: 'Zero 7 vs Mos Def - Umi Says', track: '21694756')
+
+def soundcloud_html(data)
+  html = <<-HTML
+    <iframe
+      width="100%" height="300" scrolling="no" frameborder="no"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/#{data.track}&amp;default_height=200&amp;auto_play=false&amp;hide_related=true&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;visual=true">
+    </iframe>
+  HTML
+  html.strip.gsub(/\s+/, ' ')
+end
+
+soundcloud_data.each do |data|
+  song = Song.create!(
+    title: data.title,
+    url: soundcloud_html(data)
+  )
+  ap song
+end
